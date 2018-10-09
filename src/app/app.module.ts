596bda52c5f7bc1f8ea2fe7188eaff823736ea2b
Antoine FORET <antoine.foret@epitech.eu>
2018-10-09 12:44:51 +0300

    [+] Added + button to add event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { NewPage } from "../pages/new/new";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    NewPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    NewPage
   ],
   providers: [
     StatusBar,
