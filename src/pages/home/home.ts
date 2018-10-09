ed95c11cfbcc5696902093be75fe194866b16778
Antoine FORET <antoine.foret@epitech.eu>
2018-10-09 16:06:34 +0300

    [+] Adding basic event display on homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { Storage } from "@ionic/storage";
 import { NavController } from 'ionic-angular';
 import { NewPage  } from "../new/new";
+import { EventType } from "../../constants/types";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  errorMsg = '';
+  events: EventType[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private storage: Storage) {
 
+  }
+
+  ionViewWillEnter() {
+    this.storage.get('events').then((events: EventType[]) => {
+      this.events = events;
+    }).catch(() => {
+      this.errorMsg = 'Unable to load events';
+    })
   }
 
   add() {
