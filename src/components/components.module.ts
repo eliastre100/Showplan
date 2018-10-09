1a3f8f04b0b9d1625d9976a42ded13b2fb381526
Antoine FORET <antoine.foret@epitech.eu>
2018-10-09 22:38:54 +0300

    [+] Adding show page design

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { EventItemComponent } from './event-item/event-item';
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "ionic-angular";
+import { TrackItemComponent } from './track-item/track-item';
 
 @NgModule({
-	declarations: [EventItemComponent],
+	declarations: [EventItemComponent,
+    TrackItemComponent],
 	imports: [
 	  CommonModule,
     IonicModule
   ],
-	exports: [EventItemComponent]
+	exports: [EventItemComponent,
+    TrackItemComponent]
 })
 
 export class ComponentsModule {}
--- src/pages/show/show.ts
+++ src/pages/show/show.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
+import {EventType, TrackType} from "../../constants/types";
 
 /**
  * Generated class for the ShowPage page.
@@ -13,13 +14,57 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'show.html',
 })
 export class ShowPage {
+  event: EventType;
+  tracks: TrackType[] = [
+    { artist: 'Within Temptation', name: 'She\'s in love with herself' },
+    { artist: 'Within Temptation', name: 'She likes the dark' },
+    { artist: 'Within Temptation', name: 'On her milk white neck' },
+    { artist: 'Within Temptation', name: 'The Devil\'s mark' },
+    { artist: 'Within Temptation', name: 'It\'s all Hallows Eve' },
+    { artist: 'Within Temptation', name: 'The moon is full' },
+    { artist: 'Within Temptation', name: 'Will she trick or treat' },
+    { artist: 'Within Temptation', name: 'I bet she will' },
+    { artist: 'Within Temptation', name: 'She\'s got date at midnight' },
+    { artist: 'Within Temptation', name: 'With Nosferatu' },
+    { artist: 'Within Temptation', name: 'Oh baby, Lilly Munster' },
+    { artist: 'Within Temptation', name: 'Ain\'t got nothing on you' },
+    { artist: 'Within Temptation', name: 'Well when I called her evil' },
+    { artist: 'Within Temptation', name: 'She just laughed' },
+    { artist: 'Within Temptation', name: 'And cast that spell on me' },
+    { artist: 'Within Temptation', name: 'Boo Bitch Craft' },
+    { artist: 'Within Temptation', name: 'Yeah you wanna go out \'cause it\'s raining and blowing' },
+    { artist: 'Within Temptation', name: 'You can\'t go out \'cause your roots are showing' },
+    { artist: 'Within Temptation', name: 'Dye em black' },
+    { artist: 'Within Temptation', name: 'Black no. 1' },
+    { artist: 'Within Temptation', name: 'Little wolf skin boots' },
+    { artist: 'Within Temptation', name: 'And clove cigarettes' },
+    { artist: 'Within Temptation', name: 'An erotic funeral' },
+    { artist: 'Within Temptation', name: 'For witch she\'s dressed' },
+    { artist: 'Within Temptation', name: 'Her perfume smells like' },
+    { artist: 'Within Temptation', name: 'Burning leaves' },
+    { artist: 'Within Temptation', name: 'Everyday is Halloween' },
+    { artist: 'Within Temptation', name: 'Loving you was like loving the dead' }
+  ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log(navParams.get('event'))
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private alertCtrl: AlertController) {
+    this.event = navParams.get('event');
+    if (this.event == undefined) { this.navCtrl.goToRoot({}) }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShowPage');
+  playTracklist() {
+    this.alert('track', 'track will be played on Spotify in a future update')
   }
 
+  private alert(title: string, msg: string) {
+    const alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: ['Dismiss']
+    });
+    alert.present().catch((err) => {
+      console.log(err);
+    });
+  }
 }
